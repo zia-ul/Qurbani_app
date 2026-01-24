4e5775c38e14cc6a10c870269138ab9355d28085
hajra40 <68770929+hajra40@users.noreply.github.com>
2026-01-24 12:38:10 +0530

    Comments

--- backend/routes/animallist.js
+++ backend/routes/animallist.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const isAdmin = require("../middleware/isAdmin");
 const authMiddleware = require("../middleware/authmiddleware");
+const logger = require("../middleware/logger");
 
 const {
   getAnimals,
